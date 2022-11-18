a1b1bb56d044bab3d12b40bd7b88677eac87adb9
gabrielgesteo <80053585+gabrielgesteo@users.noreply.github.com>
2022-11-18 11:22:25 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',  gotPoses);
+}
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        mouthX = results[0].pose.mouth.x;
+        mouthY = results[0].pose.mouth.y;
+    }
+}
+
+function draw()  {
+ image(video, 0, 0, 300, 300);
+ image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
